481924cc8633e54ea22eb102bc7213f9a4a43dbc
raulsync <rahul.aanand086@gmail.com>
2024-12-09 18:34:05 +0530

    feat : feedSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import userReducer from "../features/userSlice";
+import feedReducer from "../features/feedSlice";
 const store = configureStore({
   reducer: {
     user: userReducer,
+    feed: feedReducer,
   },
 });
 
