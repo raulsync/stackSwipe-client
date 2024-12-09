689be4e8476b627df882c617f9625162af62bc2b
raulsync <rahul.aanand086@gmail.com>
2024-12-09 14:51:16 +0530

    persist_user_when_refresh

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   const user = useSelector((state) => state.user);
@@ -7,7 +8,12 @@ const NavBar = () => {
   return (
     <div className="navbar bg-base-200">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">StackSwipe</a>
+        <Link
+          to={"/"}
+          className="btn btn-ghost text-xl"
+        >
+          StackSwipe
+        </Link>
       </div>
       <div className="flex-none gap-2">
         <div className="dropdown dropdown-end mx-5">
@@ -30,10 +36,13 @@ const NavBar = () => {
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
             <li>
-              <a className="justify-between">
+              <Link
+                to={"/profile"}
+                className="justify-between"
+              >
                 Profile
                 <span className="badge">New</span>
-              </a>
+              </Link>
             </li>
             <li>
               <a>Settings</a>
