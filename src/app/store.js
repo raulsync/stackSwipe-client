02aeacbeafedd3e811625ab125387c9258338afc
raulsync <rahul.aanand086@gmail.com>
2024-12-13 15:40:25 +0530

    fix: refactor_folder_structure

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import userReducer from "../features/userSlice";
 import feedReducer from "../features/feedSlice";
+import connectionReducer from "../features/connectionSLice";
+
 const store = configureStore({
   reducer: {
     user: userReducer,
     feed: feedReducer,
+    connection: connectionReducer,
   },
 });
 
